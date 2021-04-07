f9be0466158c85d8490e9fa8b96d73d6876451a6
Anna Eklund <vitastjern@gmail.com>
2021-04-07 15:31:15 +0200

    Test of all ingerited classes

--- animal_example/zoo.py
+++ animal_example/zoo.py
@@ -1,10 +1,16 @@
+# Testing the inherited classes - get_name exists only in the base class Animal
+
 from lizard import Lizard
 from gorilla import Gorilla
+from bear import Bear
+from snake import Snake
 
 lizard1 = Lizard("Sweetiepie")
 gorilla1 = Gorilla("Howard")
+bear1 = Bear("Dolly")
+snake1 = Snake("Hzzzz")
 
 print(lizard1.get_name())
-print(type(lizard1))
 print(gorilla1.get_name())
-print(type(gorilla1))
+print(bear1.get_name())
+print(snake1.get_name())
